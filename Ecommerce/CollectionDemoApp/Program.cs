597b75e34e8edfa93e8546f96e242ad1dd071979
Pratiksha Adatkar <pratiksha.adatkar@simplifyhealthcare.com>
2024-11-11 10:50:39 +0000

    Design pattern,Cloning,Indexer

---
+++ Ecommerce/CollectionDemoApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemoApp
+{
+    //containted class
+    public class book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+    }
+
+    //container class
+    public class Library
+    {
+        private List<book> books = null;
+        private string location;
+        public Library(string location)
+        {
+            books = new List<book>();
+            books.Add(new book { Id = 1, Title = "Inside C#", Description = " best seller", Author = " Steve Jobs" });
+            books.Add(new book { Id = 2, Title = "Inside Cpp", Description = " best seller", Author = " Steve Jobs" });
+            books.Add(new book { Id = 3, Title = "Inside Java", Description = " best seller", Author = " Steve Jobs" });
+            this.location = location;
+        }
+    
+        //property
+        public string Location {
+
+            get { return this.location; }
+            set { this.location = value; }
+        }
+        public book this[int index]
+        {
+            get { return books[index]; }
+            set { books[index] = value; }
+        }
+
+
+    }
+
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //library pictlib = new library();
+            string[] names = { "Sachin", "Rahul","Virat","Dhoni" };
+            string player1 = names[0];
+            string player2 = names[1];
+
+            Library pccoelib= new Library("Akurdi");
+            book thebook = pccoelib[2];
+          
+            Console.WriteLine(thebook.Title+ " "+thebook.Description+" "+thebook.Author);
+            Console.ReadLine();
+           
+        }
+    }
+}
+/* List<book> books = new List<book>();
+            List<string> list = new List<string>();
+            List<int> list2 = new List<int>();
+            */
---
+++ Ecommerce/TestDesignPatternApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDesignPatternApp
+{
+    public class complex:ICloneable
+    {
+        public int real;
+        public int img;
+        public complex()
+        {
+            real = img = 1;
+        }
+        public complex(int real, int img) {
+        this.real = real;
+        this.img = img;
+        }
+
+        public object Clone()
+        {
+            complex temp = new complex();
+            temp.real = this.real;
+            temp.img = this.img;
+            return temp;
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            complex c=new complex(1,2);
+            complex c2 = (complex)c.Clone();
+            Console.WriteLine(c2.real +" "+c2.img);
+            Console.WriteLine(c.real + " " + c.img);
+
+
+        }
+    }
+}
+/* OfficeBoy waiter = OfficeBoy.Create();
+                OfficeBoy admin= OfficeBoy.Create();
+            OfficeBoy supervisor = OfficeBoy.Create();*/
