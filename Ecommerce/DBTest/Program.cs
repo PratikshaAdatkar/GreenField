0f715f2beb3f868c7ddb3b8da878a21ad31c46b4
Pratiksha Adatkar <pratiksha.adatkar@simplifyhealthcare.com>
2024-11-28 10:52:11 +0000

    Ado.net Object Model

--- Ecommerce/DBTest/Program.cs
+++ Ecommerce/DBTest/Program.cs
@@ -1,20 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+
+
+/*
+ 
+CREATE TABLE [dbo].[products](
+	[Id] [nchar](10) NOT NULL,
+	[Name] [varchar](50) NULL,
+	[Description] [varchar](max) NULL,
+	[UnitPrice] [money] NOT NULL,
+	[Quantity] [int] NULL,
+	[Image] [varchar](max) NULL,
+ CONSTRAINT [PK_products] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+  
+ */
 
 namespace DBTestApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static string conString = @"data source=shc-sql-01.database.windows.net ; database=HangFireCatalog_VG; User Id=tmgreadonly; Password=#p7P>Wzs;";
+
+        public bool InsertRecord(int id, string name, string decription, int quantity, double unitPrice)
         {
-            string conString = @"data source=shc-sql-01.database.windows.net ; database=HangFireCatalog_VG; User Id=tmgreadonly; Password=#p7P>Wzs;";
+            bool status = false;
             IDbConnection con = new SqlConnection(conString);
-            string query = "SELECT * from products_pratiksha";
+            string query = "INSERT INTO products (Id, Name, Description, UnitPrice, Quantity, Image)"
+                           + "values(" + id + ", '" + name + "')";
+
+            IDbCommand cmd = new SqlCommand(query, con as SqlConnection);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                status = true;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return status;
+        }
+        public static void DeleteReord()
+        {
+            IDbConnection con = new SqlConnection(conString);
+            string query = "DELETE from products WHERE Id=2";
+            IDbCommand cmd = new SqlCommand(query, con as SqlConnection);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public static void ShowRecordCount()
+        {
+
+            IDbConnection con = new SqlConnection(conString);
+            string query = "SELECT COUNT(*) from products";
+            IDbCommand cmd = new SqlCommand(query, con as SqlConnection);
+            try
+            {
+                con.Open();
+                int count = int.Parse(cmd.ExecuteScalar().ToString());
+                Console.WriteLine("Records {0}", count, 56);
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+                con.Close();
+            }
+        }
+
+
+        public static void ShowAllRecords()
+        {
+
+            IDbConnection con = new SqlConnection(conString);
+            string query = "SELECT * from products";
             IDbCommand cmd = new SqlCommand(query, con as SqlConnection);
             try
             {
@@ -38,6 +129,12 @@ namespace DBTestApp
 
                 con.Close();
             }
+        }
+        static void Main(string[] args)
+        {
+
+            ShowAllRecords();
+
         }
     }
 }
